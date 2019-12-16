08b1237338e54d6bdd1fc8631ab0e078e44da635
Alessio Martignetti <al.martignetti@studenti.unina.it>
2019-12-16 18:30:11 +0100

    Push finale.

--- Prova MARTIGNETTI ALESSIO/src/it/unina/prova/test/GestoreDatiTest.java
+++ Prova MARTIGNETTI ALESSIO/src/it/unina/prova/test/GestoreDatiTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.AfterAll;
+
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
